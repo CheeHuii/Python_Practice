b1876dfd48dbce92a0333235f6d03fd9195546ba
CheeHuii <lohcheehui@gmail.com>
2024-12-16 23:35:44 +0800

    Lesson 1 and 2

---
+++ basic.py
@@ -0,0 +1,38 @@
+# _________Lesson 1____________#
+
+# Print
+print("Hello World!")
+print("This print a line to console.")
+
+# A comment!
+# Yes, this is a comment for me and you to read.
+
+
+# _________Lesson 2____________#
+# Variables (strings, integers, float, boolean)
+# Strings
+first_name = "Chee Hui"
+family_name = "Loh"
+
+# Integers
+age = 25
+
+# Float
+working_experience = 1.5
+
+# Boolean
+single = True
+
+# f mean format, f-string, allow variable in {}
+print(f"My name is {first_name}")
+print(f"I am {age} years old.")
+print(f"I have {working_experience} years of working experience.")
+print(f"Am I single? {single}")
+
+# if else statement
+if single:
+    print(":(")
+else:
+    print(":)")
+
+# _________Lesson 3____________#
